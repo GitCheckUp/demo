496cc4ba5d1a68e6beed886b511274693d0419c7
Anil Guvenc <anilguvenc@gmail.com>
2020-10-22 23:07:55 +0300

    Load file into array function
    I added a function that reads the set of random integers from the given file, then creates an integer array for it.

--- main.c
+++ main.c
@@ -3,6 +3,26 @@
 #include "shellSort.h" // shell sort header
 #include <stdio.h>
 #include <stdlib.h>
+
+int* load(int*c,char filename[]){   //Load file into array.
+	FILE *fp;
+	int temp,i,count;
+	int *toBeSorted;
+    fp=fopen(filename,"r");
+    	/*Check*/	if(fp==NULL) printf("Error at fopen()\n");
+    toBeSorted = (int*) malloc(sizeof(int));
+    fscanf(fp, "%d\n", &count);   //Read the first line to get the set size.
+    toBeSorted=(int*) malloc(sizeof(int)*(count));
+    	/*Check*/	if(toBeSorted==NULL) printf("Error: @memory allocation.\n");
+    for(i=0; i<count; i++) {
+    	fscanf(fp,"%d",&temp);
+    	toBeSorted[i]=temp;
+    }
+    *c=count; //Assign the number of entries.
+    fclose(fp);
+    return toBeSorted;
+}
+
 void main(){
 	int *x;
 	insertionSort(x,0);

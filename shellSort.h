1f3ed6d129ee964cb17747a9f9a5a86598fb9a45
Mehmet Kaan Özkan <kozkan5624@gmail.com>
2020-10-22 23:26:14 +0300

    fix: shellsort compiler dependency

--- shellSort.h
+++ shellSort.h
@@ -1,7 +1,9 @@
 void shellSort(int arr[], int n) 
 { 
-	for (int gap = n/2; gap > 0; gap/= 2){
-		for (int i = gap; i < n; i += 1){
+	int gap;
+	for (gap = n/2; gap > 0; gap/= 2){
+		int i;
+		for (i = gap; i < n; i += 1){
 			int temp = arr[i];
 			int j;
 			

759694bee08d019ef01c00dd211603b1f4a8a360
Anil Guvenc <anilguvenc@gmail.com>
2020-10-22 22:54:20 +0300

    Fix: missing header
    Manually added Shell Sort

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "insertionSort.h"  //insertion sort header
 #include "dataGeneration.h"  //USAGE: createData(integer size, "name_of_file")
-
+#include "shellSort.h" // shell sort header
 #include <stdio.h>
 #include <stdlib.h>
 void main(){
